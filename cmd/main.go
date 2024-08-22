553d94bca3dd5092237d8b7c4607c7b1a4e6eeb9
Mauro Rincon <mauro.366@gmail.com>
2024-08-22 11:29:51 -0500

    Change port in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,34 @@ import (
 	"credit-installment/internal/handlers"
 	"log"
 	"net/http"
+	"os"
 )
 
+// ------- PRDUCTION ---------
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
 	http.HandleFunc("/", handlers.FormHandler)
 	http.HandleFunc("/calculate", handlers.SubmitHandler)
 
 	// Server
-	log.Println("Run server: http://localhost:3000/")
-	if err := http.ListenAndServe("localhost:3000", nil); err != nil {
+	log.Printf("Server running on port %s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatalf("Could not start server: %s\n", err.Error())
 	}
 }
+
+// ------- DEVELOPMENT---------
+// func main() {
+// 	http.HandleFunc("/", handlers.FormHandler)
+// 	http.HandleFunc("/calculate", handlers.SubmitHandler)
+
+// 	// Server
+// 	log.Println("Run server: http://localhost:3000/")
+// 	if err := http.ListenAndServe("localhost:3000", nil); err != nil {
+// 		log.Fatalf("Could not start server: %s\n", err.Error())
+// 	}
+// }
